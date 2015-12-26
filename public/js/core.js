aa42ebb66f2e582fa6281a8c663108af39467142
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2015-12-26 16:00:51 -0600

    initial

---
+++ public/js/core.js
@@ -0,0 +1,2 @@
+//for module maheshTodo, todoController, todoservice and angular-responsive are all dependencies.
+angular.module('maheshTodo', ['todoController', 'todoService','angular-responsive']);
